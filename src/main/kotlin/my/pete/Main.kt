8b584a862ad190cbbd7be1c4fa52e3e8087afdd1
petelotadata <pete@lotadata.com>
2021-11-29 13:23:58 +0700

    split the main line to debugging

--- src/main/kotlin/my/pete/Main.kt
+++ src/main/kotlin/my/pete/Main.kt
@@ -4,7 +4,8 @@ import my.pete.game.freecell.reader.MicrosoftFreeCellGameReader
 import java.nio.file.Paths
 
 fun main() {
-    val freeCellReader = MicrosoftFreeCellGameReader(Paths.get("C:\\Users\\USER\\Pictures\\Screenshots\\sample_l.png"))
-    val game = freeCellReader.read().findBestWinGame()
-    print(game)
+    val freeCellReader = MicrosoftFreeCellGameReader(Paths.get("C:\\Users\\USER\\Pictures\\Screenshots\\sample_o.png"))
+    val game = freeCellReader.read()
+    val winGame = game.findBestWinGame()
+    print(winGame)
 }
